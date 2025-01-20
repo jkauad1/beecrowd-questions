72ac2ba98300dd66b03cc3500b36c2f146b88068
Kaua <gamersnes69@gmail.com>
2025-01-19 21:31:20 -0300

    Fixed the two commited questions

--- src/beecrowd_beginner/DDD.java
+++ src/beecrowd_beginner/DDD.java
@@ -1,4 +1,50 @@
 package beecrowd_beginner;
+import java.util.Scanner;
 
 public class DDD {
+
+    public static void main(String[] args){
+
+        Scanner reader = new Scanner(System.in);
+
+        int ddd = reader.nextInt();
+
+        if (ddd == 61){
+
+            System.out.printf("Brasilia\n");
+
+        } else if (ddd == 71) {
+
+            System.out.printf("Salvador\n");
+
+        } else if (ddd == 11) {
+
+            System.out.printf("Sao Paulo\n");
+
+        } else if (ddd == 21) {
+
+            System.out.printf("Rio de Janeiro\n");
+            
+        } else if (ddd == 32) {
+
+            System.out.printf("Juiz de Fora\n");
+
+        } else if (ddd == 19) {
+
+            System.out.printf("Campinas\n");
+
+        } else if (ddd == 27){
+
+            System.out.printf("Vitoria\n");
+
+        } else if (ddd == 31) {
+
+            System.out.printf("Belo Horizonte\n");
+
+        } else {
+
+            System.out.printf("DDD nao cadastrado\n");
+        }
+
+    }
 }
--- src/beecrowd_beginner/Taxes.java
+++ src/beecrowd_beginner/Taxes.java
@@ -1,4 +1,38 @@
 package beecrowd_beginner;
 
+import java.util.Scanner;
+
 public class Taxes {
+
+    public static void main(String[] args){
+
+        Scanner reader = new Scanner(System.in);
+
+        double salary = reader.nextDouble();
+
+        double tax = 0;
+
+        if (salary <= 2000.00){
+
+            System.out.printf("Isento\n");
+
+        } else if (salary >= 2000.01 && salary <= 3000.00) {
+
+            tax = (salary - 2000) * (8.0 / 100);
+
+        } else if (salary >= 3000.01 && salary <= 4500.00) {
+
+            tax = (1000.00 * (8.0 / 100) + (salary - 3000.01) * (18.0 / 100));
+
+        } else {
+
+            tax = (1000.00 * (8.0 / 100) + (1500 * (18.0 / 100) + (salary - 4500) * 28.0 / 100));
+
+        }
+
+         if (salary > 2000.00) {
+             System.out.printf("R$ %.2f\n", tax);
+         }
+
+    }
 }
